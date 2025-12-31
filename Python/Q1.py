8f22528c4cb1b698ad60c922597d4f773cf9a091
YashwantAID <amrita25013@gmail.com>
2025-12-31 11:26:21 +0530

    Update Q1.py

---
+++ Python/Q1.py
@@ -0,0 +1,5 @@
+##Create 4 variables of different data types (integer, float, string, boolean) that describe yourself (age, height, name, are_you_student). Print all of them with nice formattin
+age = int(input("Enter your age: "))
+height = float(input("Enter your height in meters: "))
+name = input("Enter your name: ")
+are_you_student = input("Are you a student? (yes/no): ")
