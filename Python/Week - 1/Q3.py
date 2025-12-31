b69601b14665f342fcaead4205bc8e6a22afb2fd
YashwantAID <amrita25013@gmail.com>
2025-12-31 20:01:52 +0530

    Create Q3.py

---
+++ Python/Week - 1/Q3.py
@@ -0,0 +1,8 @@
+##Create two variables: price = 599.99 and discount = 0.15
+##Calculate and print the final price after discount with 2 decimal places.
+
+price = 599.99
+discount = 0.15
+finalprice = price * (1 - discount)
+print("Final price after discount : ", finalprice)
+
