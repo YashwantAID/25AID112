d39b91e04a09ee7bfcdf67a1eeaf60c1a5c67bae
YashwantAID <amrita25013@gmail.com>
2025-12-31 11:40:32 +0530

    Update Q2.py

--- Python/Q2.py
+++ Python/Q2.py
@@ -1,5 +1,4 @@
-a = 2
-b = 5##Ask the user for their birth year (as string), convert it to integer, calculate their age in 2025 and print:"You will be X years old in 2025!" (X = 2025 - birth_year)
+##Ask the user for their birth year (as string), convert it to integer, calculate their age in 2025 and print:"You will be X years old in 2025!" (X = 2025 - birth_year)
 birth_year = input("Enter your birth year: ")
 birth_year = int(birth_year)
 age_in_2025 = 2025 - birth_year    
