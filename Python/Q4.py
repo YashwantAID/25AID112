04ddde3b1f729c54663f1246ebd293a3433ae21a
YashwantAID <amrita25013@gmail.com>
2025-12-31 19:53:32 +0530

    Favourite Number

--- Python/Q4.py
+++ Python/Q4.py
@@ -1,4 +1,4 @@
-##Create a variable with your favourite number.
+##Create a variable with your favourite number..
 ##Print whether this number is even or odd (use % operator).
 
 favourite_number = int(input("Enter your favourite number: "))
