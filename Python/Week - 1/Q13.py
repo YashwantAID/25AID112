5d30daf8c0612c2e71d404cf2b96814bd4abdfa4
YashwantAID <amrita25013@gmail.com>
2025-12-31 20:02:29 +0530

    Create Q13.py

---
+++ Python/Week - 1/Q13.py
@@ -0,0 +1,9 @@
+##Create a list with 6 different food names..
+##Use a for loop to print each food with numbering:
+
+food_list = ["Pizza", "Burger", "Pasta", "Sushi", "Salad", "Tacos"] 
+for i in range(len(food_list)):
+    print(i + 1, food_list[i])
+
+
+    
