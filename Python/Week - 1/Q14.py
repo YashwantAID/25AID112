9d1e36a1e75f1544ebbccd03f0775266e44508a6
YashwantAID <amrita25013@gmail.com>
2025-12-31 20:02:32 +0530

    Create Q14.py

---
+++ Python/Week - 1/Q14.py
@@ -0,0 +1,8 @@
+##Create an empty list.Use a loop to ask user 5 times to enter their favourite movies.
+##Add each movie to the list.After all inputs, print the whole list..
+
+favourite_movies = []
+for i in range(5):
+    movie = input("Enter a favourite movie: ")
+    favourite_movies.append(movie)
+print("Your favourite movies are:", favourite_movies)
