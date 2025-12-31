55d4b325655c89e3b1e2d8edd463dd5c5f020b80
YashwantAID <amrita25013@gmail.com>
2025-12-31 11:32:11 +0530

    Update Q1.py

--- Python/Q1.py
+++ Python/Q1.py
@@ -3,3 +3,10 @@ age = int(input("Enter your age: "))
 height = float(input("Enter your height in meters: "))
 name = input("Enter your name: ")
 are_you_student = input("Are you a student? (yes/no): ")
+print("Name:", name)
+print("Age:", age)         
+print("Height:", height, "meters")  
+if are_you_student.lower() == 'yes':
+    print("Student: True") 
+else:
+    print("Student: False")
